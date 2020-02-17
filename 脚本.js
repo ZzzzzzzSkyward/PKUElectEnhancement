6b163db388b5689e0cc13a83c26da92f53c6cf4f
zzzzzzzs <55184223+ZzzzzzzSkyward@users.noreply.github.com>
2020-02-17 18:26:51 +0800

    Create 脚本.js

---
+++ 脚本.js
@@ -0,0 +1,30 @@
+var isHidden=1;//此行可修改
+var college="某个学院"//此行可修改，初衷是删掉所以已经选掉的培养方案课程
+var zz;
+
+for(zz of document.getElementsByTagName('span')){
+
+if(zz.innerText){
+
+if(zz.innerText.match(/([0-9]{2,3}) \/ \1/)){
+
+if(!isHidden){
+
+zz.parentElement.parentElement.style.opacity='0.5'
+
+}else{
+
+zz.parentElement.parentElement.style.display='none';}
+
+}else if(zz.innerText==){
+
+if(!isHidden){
+
+zz.parentElement.parentElement.style.opacity="0.5";
+
+}else{
+
+zz.parentElement.parentElement.style.display='none';
+
+
+}}}}
